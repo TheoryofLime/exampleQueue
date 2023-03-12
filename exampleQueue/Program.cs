446458ce10616acec4cce05dd21e0e2e0ae8f9e2
user <user@PC>
2023-03-12 18:03:10 -0400

    Add project files.

---
+++ exampleQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exampleQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <string> newQueue = new Queue<string>();
+            // Add items
+            newQueue.Enqueue("A");
+            newQueue.Enqueue("B");
+            newQueue.Enqueue("C");
+            newQueue.Enqueue("D");
+            newQueue.Enqueue("E");
+            // Checking for a specific element
+            Console.WriteLine(newQueue.Contains("C"));
+            // Removing an object
+            newQueue.Dequeue();
+            // Counting total
+            Console.WriteLine(newQueue.Count());
+            // print all that LOOT!
+            foreach (string s in newQueue)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
